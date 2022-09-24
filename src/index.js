fc41815cdf1729cf86f83058146ab834b5cf9222
drunkzhuo <2745914927@qq.com>
2022-09-24 19:02:30 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './css/style.scss'
 import png from './assets/image/1.png'
 
 const div = document.getElementById('app')
-div.innerHTML = `
-    <img src="${png}">
-`
+    // div.innerHTML = `
+    //     <img src="${png}">
+    // `
 
 const button = document.createElement('button')
 button.innerHTML = '懒加载'
